f9116900cd57a24040dcdf0aa83f9f298b92c732
Murat Ilgun <muratilgun34@hotmail.com>
2020-12-09 12:40:08 +0300

    NHibernate implementasyonu yapıldı.

---
+++ LudFramework.Core/DataAccess/NHibernate/NhQueryableRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LudFramework.Core.Entities;
+
+namespace LudFramework.Core.DataAccess.NHibernate
+{
+    public class NhQueryableRepository<T>:IQueryableRepository<T> where T:class,IEntity,new()
+    {
+        private NHibernateHelper _nHibernateHelper;
+        private IQueryable<T> _entities;
+
+        public NhQueryableRepository(NHibernateHelper nHibernateHelper)
+        {
+            _nHibernateHelper = nHibernateHelper;
+        }
+
+        public IQueryable<T> Table // => this.Entities;
+        {
+            get
+            {
+                return this.Entities;
+            }
+        }
+
+        public virtual IQueryable<T> Entities
+        {
+            get // { return _entities ?? (_entities = _nHibernateHelper.OpenSession().Query<T>()); }
+            {
+                if (_entities==null)
+                {
+                    _entities = _nHibernateHelper.OpenSession().Query<T>();
+                }
+
+                return _entities;
+            }
+        }
+    }
+}
