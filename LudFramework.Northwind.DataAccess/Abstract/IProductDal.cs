c391bade8f3828f95d093bab1e15f1287f326bbd
Murat Ilgun <muratilgun34@hotmail.com>
2020-12-09 16:34:56 +0300

    ComplexType oluşturuldu. Product ve Category join

--- LudFramework.Northwind.DataAccess/Abstract/IProductDal.cs
+++ LudFramework.Northwind.DataAccess/Abstract/IProductDal.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using LudFramework.Core.DataAccess;
+using LudFramework.Northwind.Entities.ComplexTypes;
 using LudFramework.Northwind.Entities.Concrete;
 
 namespace LudFramework.Northwind.DataAccess.Abstract
 {
     public interface IProductDal:IEntityRepository<Product>
     {
-        
+        List<ProductDetail> GetProductDetails();
     }
 }
--- LudFramework.Northwind.DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ LudFramework.Northwind.DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -6,11 +6,29 @@ using System.Text;
 using System.Threading.Tasks;
 using LudFramework.Core.DataAccess.EntityFramework;
 using LudFramework.Northwind.DataAccess.Abstract;
+using LudFramework.Northwind.Entities.ComplexTypes;
 using LudFramework.Northwind.Entities.Concrete;
 
 namespace LudFramework.Northwind.DataAccess.Concrete.EntityFramework
 {
     public class EfProductDal : EfEntityRepositoryBase<Product, NorthwindContext>, IProductDal
     {
+        public List<ProductDetail> GetProductDetails()
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var result = from p in context.Products
+                    join c in context.Categories on p.CategoryId equals c.CategoryId
+                    select new ProductDetail
+                    {
+                        ProductId = p.ProductId,
+                        ProductName = p.ProductName,
+                        CategoryName = c.CategoryName
+
+                    };
+                return result.ToList();
+            }
+            
+        }
     }
 }
