ed78136551c279ae1ce7be0e1e7c3194e7434f0a
Murat Ilgun <muratilgun34@hotmail.com>
2020-12-09 12:09:44 +0300

    Entity framework implementasyonu yapıldı

---
+++ LudFramework.Core/DataAccess/EntityFramework/EfQueryableRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LudFramework.Core.Entities;
+
+namespace LudFramework.Core.DataAccess.EntityFramework
+{
+    public class EfQueryableRepository<T>:IQueryableRepository<T> where T: class,IEntity,new()
+    {
+        private DbContext _context;
+        private IDbSet<T> _entities;
+
+        public EfQueryableRepository(DbContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<T> Table // => this.Entities;
+        {
+            get { return this.Entities; }
+        }
+
+        protected virtual IDbSet<T> Entities
+        {
+            get // get { return _entities ?? (_entities = _context.Set<T>()); }
+            {
+                if (_entities==null)
+                {
+                    _entities = _context.Set<T>();
+                }
+
+                return _entities;
+            }
+        }
+    }
+}
--- LudFramework.Core/Entities/IEntity.cs
+++ LudFramework.Core/Entities/IEntity.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LudFramework.Core
+namespace LudFramework.Core.Entities
 {
-    public class Class1
+    public interface IEntity
     {
     }
 }
